16dd900b294f9e242d89725e15722eb69f2e135c
Vincent Cornière <vincent.corniere@gmail.com>
2016-02-06 22:32:23 +0100

    cap produc and staging ok

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -30,7 +30,7 @@ role :db,  %w{dezande@vincent-corniere.fr}
 # For available Capistrano configuration variables see the documentation page.
 # http://capistranorb.com/documentation/getting-started/configuration/
 # Feel free to add new variables to customise your setup.
-
+set :deploy_to, '/var/www/dezande/production'
 
 
 # Custom SSH Options
