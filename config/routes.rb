dc3228f2bb1c817cf710b8b8dc4b2a002c63a013
Vincent Cornière <vincent.corniere@gmail.com>
2016-02-08 20:04:32 +0100

    remove controller skills and contact

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-
-  # menu top
   resources :homepage, only: :index
-  resources :skills, only: :index
-  resources :contact, only: :index
 
   root 'homepage#index'
 end
--- test/controllers/skills_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class SkillsControllerTest < ActionController::TestCase
-  test "Should get index" do
-    get :index
-    assert_response :success
-  end
-end
