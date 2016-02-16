4b1d531d6ab70410f511217d2ac8f9eba7ca9846
Vincent Cornière <vincent.corniere@gmail.com>
2016-02-16 22:39:48 +0100

    add config application

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ require "action_mailer/railtie"
 require "action_view/railtie"
 require "sprockets/railtie"
 require "rails/test_unit/railtie"
+require 'mongoid'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -28,6 +29,8 @@ module Dezande
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :fr
+
+    Mongoid.load!(File.expand_path('mongoid.yml', './config'))
 
     # Configure fallbacks for mongoid errors:
     require "i18n/backend/fallbacks"
