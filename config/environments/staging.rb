1204a1a4a08a94419359d5870156f11ddc20b524
Vincent Cornière <vincent.corniere@gmail.com>
2016-02-23 23:06:23 +0100

    config mail

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -73,4 +73,15 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # Config mail
+  config.action_mailer.delivery_method = :sendmail
+  # Defaults to:
+  # config.action_mailer.sendmail_settings = {
+  #   location: '/usr/sbin/sendmail',
+  #   arguments: '-i -t'
+  # }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: 'contact@vincent-corniere.fr'}
 end
