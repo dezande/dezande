4cdace1c0c19ba8e04f1046c8f58748756887d1f
Vincent Cornière <vincent.corniere@gmail.com>
2016-03-05 03:53:24 +0100

    change ip adresse

--- config/application.rb
+++ config/application.rb
@@ -43,6 +43,6 @@ module Dezande
     end
 
     # Configure redis
-    config.cache_store = :redis_store, "redis://0.0.0.0:6379/0/cache", { expires_in: 90.minutes }
+    config.cache_store = :redis_store, "redis://127.0.0.1:6379/0/cache", { expires_in: 90.minutes }
   end
 end
